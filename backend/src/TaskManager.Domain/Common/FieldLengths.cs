72485bf062b1e2146e7df4820bfa2a2a65665b1c
Ahmed Ezz <Ahmed.Ezz@premiumcard.net>
2026-07-28 18:04:04 +0300

    feat(domain): add project and task entities

---
+++ backend/src/TaskManager.Domain/Common/FieldLengths.cs
@@ -0,0 +1,8 @@
+namespace TaskManager.Domain.Common;
+
+public static class FieldLengths
+{
+    public const int ProjectName = 100;
+    public const int TaskTitle = 200;
+    public const int Description = 1000;
+}
